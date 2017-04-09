6dfbf73ee4cd13f8f135b57ca1078a6dc57c20f2
doomnuggets <doomnuggets@users.noreply.github.com>
2017-04-09 11:22:32 +0200

    do the fucking thing with the dynamic sizing

--- a-fucking-rainbow.c
+++ a-fucking-rainbow.c
@@ -17,22 +17,24 @@ int main(int argc, char *argv[])
     char blue[] = "\e[48;5;27m";
     char indigo[] = "\e[48;5;57m";
     char violet[] = "\e[48;5;54m";
+    char reset_the_fucking_color[] = "\e[49m";
 
     char *all_the_fucking_colors[] = {red, orange, yellow, green, blue, indigo, violet};
     int number_of_all_the_fucking_colors = 6;
     int the_current_fucking_color = 0;
 
     struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int rainbow_height = (int)(w.ws_col / 21);
 
     while(1)
     {
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+        int rainbow_height = (int)(w.ws_col / 30);
+        int rainbow_width = w.ws_col;
         for(int row = 0; row < rainbow_height; row++)
         {
-            for(int i = 0; i < w.ws_col; i++)
+            for(int col = 0; col < rainbow_width; col++)
             {
-                printf("%s%c\e[49m", all_the_fucking_colors[the_current_fucking_color], ' ');
+                printf("%s%c%s", all_the_fucking_colors[the_current_fucking_color], ' ', reset_the_fucking_color);
             }
             printf("\n");
         }
